8f76e191ce47e079974cd746ad53a5541d2d1875
Fathima thasnim <thasnimfathima492@gmail.com>
2022-01-18 16:02:02 -0500

    refractor button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,25 +1,24 @@
 import React from "react";
 
 import "components/Button.scss";
+import classNames from "classnames";
+
 
 export default function Button(props) {
-   let buttonClass = "button";
+   const buttonClass = classNames("button", {
+      "button--confirm": props.confirm,
+      "button--danger": props.danger
+   });
 
-   if (props.confirm) {
-      buttonClass += " button--confirm";
-   } 
+   
 
-   if (props.danger) {
-      buttonClass += " button--danger";
-   }
-
-   return (
+return (
    <button 
    
    onClick={props.onClick} 
    disabled={props.disabled} 
    className={buttonClass}
-   
+
    >
       {props.children}
    </button>
