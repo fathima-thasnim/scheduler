c034af4d0d20bf235984149d5f0b136c7765e2d0
Fathima thasnim <thasnimfathima492@gmail.com>
2022-01-19 15:15:39 -0500

    build interviewer item

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import "components/InterviewerListItem.scss";
+
+import classNames from "classnames";
+
+export default function InterviewerListItem(props) {
+  const { name, avatar, setInterviewer} = props;
+
+  let interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+  });
+   
+  return (
+    <li onClick={setInterviewer} className={interviewerClass}>
+      <img
+        className="interviewers__item-image"
+        src={avatar}
+        alt={name}
+      />
+      Sylvia Palmer
+    </li>
+  );
+}
